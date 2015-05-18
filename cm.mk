8ec8157498d114d4008c64a4297766f78765a6f5
dark_wizard <nightphoenix11@gmail.com>
2015-05-18 13:28:41 +0300

    mkvendor.sh: Initial commit of z2

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := z2
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/z2/device_z2.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := z2
+PRODUCT_NAME := cm_z2
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := z2
+PRODUCT_MANUFACTURER := lge
